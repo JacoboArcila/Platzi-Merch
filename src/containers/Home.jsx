dc30bad08680cb11cc1a78aec40180ac05d5ae23
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2021-05-14 22:33:03 -0500

    mi primer commit

---
+++ src/containers/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import initialState from '../initialState';
+import Products from '../components/Products';
+
+function Home() {
+  return (
+    <Products Products={initialState.products} />
+  )
+};
+
+export default Home;
